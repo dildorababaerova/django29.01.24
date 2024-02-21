acd076f09e40dcf5b302cc4fc288fd1988eb4c4c
Dildora <48212@edu.raseko.fi>
2024-02-21 09:01:22 +0200

    lisätty

--- kysely/admin.py
+++ kysely/admin.py
@@ -3,10 +3,18 @@ from django.contrib import admin
 from .models import Kysymys, Vaihtoehto
  
 
-admin.site.register(Kysymys)
+
+@admin.register(Kysymys)
+class KysymysAdmin(admin.ModelAdmin):
+    fields = ["julkaisupvm", "teksti"]
+
+
 
 
 @admin.register(Vaihtoehto)
 class VaihtoehtoAdmin(admin.ModelAdmin):
     list_display = ["kysymys", "teksti"]
+
+
+
 
